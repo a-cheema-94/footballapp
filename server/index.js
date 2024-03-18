d4dec84128e729136834428ed7dcfa2bc0687b26
Amar Cheema <amr.cheema@yahoo.com>
2024-03-18 15:07:40 +0000

    apollo server setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+});
+
+console.log('graphql server ready at port: ', 4000);
